44347242588f2d4e446c4b645327bccb2c2c9686
Wadah Esam <wadah-wleed@hotmail.com>
2018-09-17 22:14:27 +0300

    installing web3 and connecting it with the provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import web3 from './web3';
 
 class App extends Component {
   render() {
+
+    // printing the version of the web3 that we created
+    console.log('WEB 3 Version : ' + web3.version);
+    web3.eth.getAccounts()
+      .then( accounts => {  
+        console.log('The Metamax Account : ' + accounts);
+        web3.eth.getBalance(accounts[0])
+        .then( balance => {
+          console.log('Current Balance in the accounts in wei : ' + balance);
+        });
+      });
+   
+
     return (
       <div className="App">
         <header className="App-header">
